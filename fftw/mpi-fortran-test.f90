5f88cc436ea49e4e258f784ce8eaa6c6f4d0ad5c
Alberto <alberto.popescu@stfc.ac.uk>
2021-11-08 10:53:36 +0000

    change mpi-fortran-test.f90

--- fftw/mpi-fortran-test.f90
+++ fftw/mpi-fortran-test.f90
@@ -1,6 +1,7 @@
 program test
-
-use, intrinsic :: iso_c_binding
+  
+  use, intrinsic :: iso_c_binding
+  include 'fftw3.h'
   include 'fftw3-mpi.f03'
   integer(C_INTPTR_T), parameter :: L = 100
   integer(C_INTPTR_T), parameter :: M = 100
@@ -9,16 +10,16 @@ use, intrinsic :: iso_c_binding
   integer(C_INTPTR_T) :: i, j, alloc_local, local_M, local_j_offset
   integer ierror
   call MPI_init(ierror)
-  call fftw_mpi_init
+  call fftwl_mpi_init
 
 !   get local data size and allocate (note dimension reversal)
-  alloc_local = fftw_mpi_local_size_2d(M, L, MPI_COMM_WORLD, &
+  alloc_local = fftwl_mpi_local_size_2d(M, L, MPI_COMM_WORLD, &
                                        local_M, local_j_offset)
-  cdata = fftw_alloc_complex(alloc_local)
+  cdata = fftwl_alloc_complex(alloc_local)
   call c_f_pointer(cdata, data, [L,local_M])
 
 !   create MPI plan for in-place forward DFT (note dimension reversal)
-  plan = fftw_mpi_plan_dft_2d(M, L, data, data, MPI_COMM_WORLD, &
+  plan = fftwl_mpi_plan_dft_2d(M, L, data, data, MPI_COMM_WORLD, &
                               FFTW_FORWARD, FFTW_MEASURE)
 
 ! initialize data to some function my_function(i,j)
@@ -29,9 +30,9 @@ use, intrinsic :: iso_c_binding
   end do
 
 ! compute transform (as many times as desired)
-  call fftw_mpi_execute_dft(plan, data, data)
+  call fftwl_mpi_execute_dft(plan, data, data)
 
-  call fftw_destroy_plan(plan)
-  call fftw_free(cdata)
+  call fftwl_destroy_plan(plan)
+  call fftwl_free(cdata)
   call MPI_FINALIZE(ierror)
 end
